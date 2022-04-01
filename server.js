00dc2199f075e46983998cd0650f4b1a30661f0e
Benny623 <bmilner88@gmail.com>
2022-04-01 15:10:39 -0600

    got site working

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { db } = require('./db/db.json');
+//const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+//app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
