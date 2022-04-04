0b4fe180f06a6d26332dbbfc67874da46e701171
Benny623 <bmilner88@gmail.com>
2022-04-04 14:38:26 -0600

    added delete..? maybe?

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,16 +3,40 @@ const path = require('path');
 const router = require('express').Router();
 const db = require('../../db/db.json');
 
+// API GET request
 router.get('/notes', (req, res) => {
     res.json(db);
 });
 
+// API POST request
 router.post('/notes', (req, res) => {
     const note = req.body;
     const notes = db;
+
+    note.id = notes.length + 1;
+
     notes.push(note);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes), 'utf-8');
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+
     res.json(db);
+});
+
+// API DELETE request
+router.delete('/notes/:id', (req, res) => {
+    const delId = parseInt(req.params.id);
+    const notes = db;
+    const keepNotes = [];
+
+    notes.forEach(note => {
+        if(note.id !== delId) {
+            keepNotes.push(note);
+        }
+    });
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(keepNotes));
+
+    res.json(db)
 });
 
 module.exports = router;
