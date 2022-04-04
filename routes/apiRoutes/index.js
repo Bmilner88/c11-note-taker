d7b209a6c0c2b1392bff526a611cf541255f6841
Benny623 <bmilner88@gmail.com>
2022-04-04 16:26:23 -0600

    finally something that works

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,37 +3,51 @@ const util = require('util');
 const router = require('express').Router();
 const db = require('../../db/db.json');
 
+const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // API GET request
 router.get('/notes', (req, res) => {
-    res.json(db);
+    readFileAsync('./db/db.json', 'utf-8').then(data => {
+        const notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    });
 });
 
 // API POST request
 router.post('/notes', (req, res) => {
     const note = req.body;
-    const notes = db;
-
-    note.id = notes.length + 1;
-
-    notes.push(note);
-
-    writeFileAsync('./db/db.json', JSON.stringify(notes));
-
-    res.json(db);
+    readFileAsync('./db/db.json', 'utf-8')
+        .then(data => {
+            const notes = [].concat(JSON.parse(data));
+            note.id = notes.length + 1;
+            notes.push(note);
+            return notes;
+        }).then(notes => {
+            writeFileAsync('./db/db.json', JSON.stringify(notes))
+            res.json(note);
+        });
 });
 
 // API DELETE request
 router.delete('/notes/:id', (req, res) => {
     const delId = parseInt(req.params.id);
-    const keepNotes = db.filter(note => note.id !== delId);
+    readFileAsync('./db/db.json', 'utf-8')
+        .then(data => {
+            const notes = [].concat(JSON.parse(data));
+            const keepNotes = [];
 
+            notes.forEach(note => {
+                if(note.id !== delId) {
+                    keepNotes.push(note);
+                };
+            });
 
-        writeFileAsync('./db/db.json', JSON.stringify(keepNotes))
-
-
-    res.json(db);
+            return keepNotes;
+        }).then(notes => {
+            writeFileAsync('./db/db.json', JSON.stringify(notes))
+            res.send('DELETE request received');
+        });
 });
 
 module.exports = router;
