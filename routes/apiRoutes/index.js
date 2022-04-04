a2c4694893a9d4e512c3489845c6cfdacce30046
Benny623 <bmilner88@gmail.com>
2022-04-04 17:59:12 -0600

    added unique id handling and functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const router = require('express').Router();
+const uuid = require('../../helpers/uuid');
 
 // API GET request
 router.get('/notes', (req, res) => {
@@ -11,18 +12,33 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const note = req.body;
     const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
-        
-    note.id = notes.length + 1;
-    notes.push(note);
-            
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    const idArr = [];
+
+    notes.forEach(note => {
+        idArr.push(note.id);
+    });
+    console.log(idArr);
+    
+    note.id = uuid();
+
+    for(i = 0; i < idArr.length; i++) {
+        if(note.id !== idArr[i]) {
+            notes.push(note);
+            break;
+        } else {
+            note.id = uuid();
+        };
+    };
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, 2));
     
     res.json(note);
 });
 
 // API DELETE request
 router.delete('/notes/:id', (req, res) => {
-    const delId = parseInt(req.params.id);
+    const delId = req.params.id;
+    console.log(delId)
     const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
     const keepNotes = [];
 
@@ -32,7 +48,7 @@ router.delete('/notes/:id', (req, res) => {
         };
     });
 
-    fs.writeFileSync('./db/db.json', JSON.stringify(keepNotes))
+    fs.writeFileSync('./db/db.json', JSON.stringify(keepNotes, null, 2));
     res.send('DELETE request received');
 });
 
